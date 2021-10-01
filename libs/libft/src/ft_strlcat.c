99e40b95d6e68386a7c738536e56ca7814369297
safoh <safoh@student.codam.nl>
2021-10-01 17:52:38 +0200

    still struggling with basic warmachine test

--- libs/libft/src/ft_strlcat.c
+++ libs/libft/src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: saladin <saladin@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/29 02:51:19 by saladin       #+#    #+#                 */
-/*   Updated: 2021/07/26 15:34:31 by safoh         ########   odam.nl         */
+/*   Updated: 2021/10/01 17:52:26 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	d_len;
 	size_t	s_len;
-	size_t	res;
 	size_t	i;
 
-	d_len = ft_strlen(dst);
 	s_len = ft_strlen(src);
-	res = 0;
+	d_len = ft_strlen(dst);
+	if (size - 1 <= d_len)
+		return (s_len + size);
 	i = 0;
-	if (size == 0)
-		return (s_len);
-	if (size > d_len)
-		res = d_len + s_len;
-	else
-		res = s_len + size;
 	while (i + d_len < size - 1 && src[i])
 	{
 		dst[i + d_len] = src[i];
 		i++;
 	}
 	dst[i + d_len] = '\0';
-	return (res);
+	return (d_len + s_len);
 }

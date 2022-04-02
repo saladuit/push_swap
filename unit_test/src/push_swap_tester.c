392b7491a3ca015dbba2f6ff0a9bd2e1664a4db9
safoh <safoh@student.codam.nl>
2022-04-02 18:26:23 +0200

    fixed almost all parsing tests

--- unit_test/src/push_swap_tester.c
+++ unit_test/src/push_swap_tester.c
@@ -6,31 +6,38 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/03/09 20:05:09 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/03/29 17:39:55 by safoh        \___)=(___/                 */
+/*   Updated: 2022/04/02 16:21:23 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "/Users/safoh/Documents/Projects_Codam/push_swap/unit_test/include/unit_push_swap.h"
+#include "/Users/safoh/Documents/Projects_Codam/push_swap/libs/libft/include/libft.h"
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include <criterion/internal/assert.h>
 #include <criterion/logging.h>
 #include <string.h>
-/*#include "unit_push_swap.h"*/
 
 /* ************************************************************************** */
 TestSuite(check_argc, .init=redirect_all_stdout);
+/* ************************************************************************** */
 
-Test(check_argc, one_arg, .fini=assert_empty_stdout)
+Test(check_argc, one_arg)
 {
 	cr_expect(count_check(1) == false);
+	cr_assert_stdout_eq_str("");
 }
 
-Test(check_argc, two_arg, .fini=assert_error_stdout)
+Test(check_argc, two_arg)
 {
 	cr_expect(count_check(2) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argc, more_than_two_arg, .fini=assert_empty_stdout)
+Test(check_argc, more_than_two_arg)
 {
 	cr_expect(count_check(3) == true);
+	cr_assert_stdout_eq_str("");
 }
 /* ************************************************************************** */
 /*check for:*/
@@ -41,141 +48,302 @@ Test(check_argc, more_than_two_arg, .fini=assert_empty_stdout)
 /*- no parameters sould display nothing*/
 /*- check "-"*/
 TestSuite(check_argv, .init=redirect_all_stdout);
+/* ************************************************************************** */
 
-Test(check_argv, NULL_pointer, .fini=assert_empty_stdout)
+Test(check_argv, NULL_pointer)
 {
 	cr_expect(var_check(0, NULL) == false);
 }
 
-Test(check_argv, one_number, .fini=assert_empty_stdout)
+Test(check_argv, one_number)
 {
 	char *string[] = {"1", NULL};
 	cr_expect(var_check(1, string) == true);
+	cr_assert_stdout_eq_str("");
 }
 
-Test(check_argv, one_char, .fini=assert_error_stdout)
+Test(check_argv, one_char)
 {
 	char *string[] = {"a", NULL};
 	cr_expect(var_check(1, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, two_numbers_sorted, .fini=assert_empty_stdout)
+Test(check_argv, two_numbers_sorted)
 {
 	char *string[] = {"1", "2", NULL};
 	cr_expect(var_check(2, string) == true);
+	cr_assert_stdout_eq_str("");
 }
 
-Test(check_argv, two_numbers_unsorted, .fini=assert_empty_stdout)
+Test(check_argv, two_numbers_unsorted)
 {
 	char *string[] = {"2", "1", NULL};
 	cr_expect(var_check(2, string) == true);
+	cr_assert_stdout_eq_str("");
 }
 
-Test(check_argv, invalid_char, .fini=assert_error_stdout)
+Test(check_argv, invalid_char)
 {
 	char *string[] = {"a", "1", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_char_second_arg, .fini=assert_error_stdout)
+Test(check_argv, invalid_char_second_arg)
 {
 	char *string[] = {"1", "a", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_char_second_position, .fini=assert_error_stdout)
+Test(check_argv, invalid_char_second_position)
 {
 	char *string[] = {"1a", "1", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
-Test(check_argv, invalid_char_random_position, .fini=assert_error_stdout)
+Test(check_argv, invalid_char_random_position)
 {
 	char *string[] = {"a1", "1234a", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_char_nl, .fini=assert_error_stdout)
+Test(check_argv, invalid_char_nl)
 {
 	char *string[] = {"\n", "2", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_char_specialchar, .fini=assert_error_stdout)
+Test(check_argv, invalid_char_specialchar)
 {
 	char *string[] = {"1", "-", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_char_tab, .fini=assert_error_stdout)
+Test(check_argv, invalid_char_tab)
 {
 	char *string[] = {"1", "\t", NULL};
 	cr_expect(var_check(2, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_nullterminator, .fini=assert_error_stdout)
+Test(check_argv, invalid_nullterminator)
 {
 	char *string[] = {"4", "\0", "3", NULL};
-	cr_expect(var_check(3, string) == false);
+	cr_expect(var_check(3, string) == true);
+	cr_assert_stdout_eq_str("");
 }
 
-Test(check_argv, invalid_nullterminator_check, .fini=assert_empty_stdout)
+Test(check_argv, invalid_nullterminator_check)
 {
 	char *string[] = {"4", "0", "3", NULL};
 	cr_expect(var_check(3, string) == true);
+	cr_assert_stdout_eq_str("");
 }
 
-Test(check_argv, invalid_3x_nullterminator, .fini=assert_error_stdout)
+Test(check_argv, invalid_3x_nullterminator)
 {
 	char *string[] = {"\\0", "2", "\0", NULL};
 	cr_expect(var_check(3, string) == false);
+	cr_expect_stdout_eq_str("error\n");
 }
 
-Test(check_argv, invalid_nullterminator_afternum, .fini=assert_empty_stdout)
+Test(check_argv, invalid_nullterminator_afternum)
 {
 	char *string[] = {"4", "1\0", "0", NULL};
 	cr_expect(var_check(3, string) == true);
+	cr_assert_stdout_eq_str("");
 }
 
-TestSuite(init_stack_a, .init=redirect_all_stdout);
+/* ************************************************************************** */
+TestSuite(argvtoarray, .init=redirect_all_stdout);
+/* ************************************************************************** */
 
-Test(init_stack_a, null_pointer, .init=assert_empty_stdout)
+Test(argvtoarray, NULL_values)
 {
-	cr_expect(init_stack_a(0, NULL) == NULL);
+	cr_assert(argvtoarray(0, NULL, NULL) == NULL);
+	cr_assert_stdout_eq_str("Error\n");
 }
 
-Test(init_stack_a, one_valid_char, .init=assert_empty_stdout)
+Test(argvtoarray, one_valid_argv)
 {
-	char *string[] = {"1", NULL};
-	t_list	*stack_a;
-	int argc;
-	size_t i;
+	cr_assert(argvtoarray(0, NULL, NULL) == NULL);
+	cr_assert_stdout_eq_str("Error\n");
+}
+/*Is the list already sorted and does it contain double numbers*/
+/* ************************************************************************** */
+TestSuite(array_check, .init=redirect_all_stdout);
+/* ************************************************************************** */
 
-	argc = 1;
-	i = 0;
-	stack_a = init_stack_a(argc, string);
-	while(i < (size_t)argc)
-	{
-		cr_expect(strcmp(stack_a->content, string[i]) == 0);
-		stack_a = stack_a->next;
-		i++;
-	}
+bool	test_array_check(int *expected)
+{
+	int len;
+
+	len = 0;
+	if (expected)
+		while (expected[len])
+			len++;
+	if (array_check(len, expected) == false)
+		return (false);
+	return (true);
 }
 
-Test(init_stack_a, two_valid_char, .init=assert_empty_stdout)
+Test(array_check, NULL_values)
 {
-	char *string[] = {"1", "2", NULL};
+	cr_assert(test_array_check(NULL) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
+
+Test(array_check, one_nemb_sorted)
+{
 	int	expected[] = {1, 2, '\0'};
-	t_list	*stack_a;
-	int argc;
-	size_t i;
+	cr_assert(test_array_check(expected) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
 
-	argc = 2;
-	i = 0;
-	stack_a = init_stack_a(argc, string);
-	while(i < (size_t)argc)
+Test(array_check, two_nemb_unsorted)
+{
+	int	expected[] = {2, 1, '\0'};
+	cr_assert(test_array_check(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(array_check, two_nemb_double)
+{
+	int	expected[] = {2, 2, '\0'};
+	cr_assert(test_array_check(expected) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
+
+Test(array_check, three_nemb_double_middle)
+{
+	int	expected[] = {2, 3, 2, '\0'};
+	cr_assert(test_array_check(expected) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
+
+Test(array_check, three_nemb_double_back)
+{
+	int	expected[] = {3, 2, 2, '\0'};
+	cr_assert(test_array_check(expected) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
+
+Test(array_check, three_nemb_double_front)
+{
+	int	expected[] = {2, 2, 3, '\0'};
+	cr_assert(test_array_check(expected) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
+
+
+/* ************************************************************************** */
+TestSuite(init_stack_a, .init=redirect_all_stdout);
+/* ************************************************************************** */
+
+bool	test_init_stack_a(int *expected)
+{
+	t_list	*stack_a = NULL;
+	int number;
+	int len = 0;
+	size_t i = 0;
+
+	if (expected)
+		while (expected[len])
+			len++;
+	stack_a = init_stack_a(len, expected, stack_a);
+	if (stack_a == NULL)
+		return (false);
+	while(i < (size_t)len)
 	{
-		cr_expect(((int*)(stack_a->content)) == 1);
+		number = *(int *)stack_a->content;
+		if(number != expected[i])
+			return (false);
 		stack_a = stack_a->next;
 		i++;
 	}
+	ft_lstclear(&stack_a, NULL);
+	return (true);
+}
+
+Test(init_stack_a, null_pointer)
+{
+	int *expected = NULL;
+	
+	cr_assert(test_init_stack_a(expected) == false);
+	cr_assert_stdout_eq_str("Error\n");
+}
+
+Test(init_stack_a, one_valid_int)
+{
+	int	expected[] = {1, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, MAX_int)
+{
+	int	expected[] = {2147483647, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, double_MAXMAX_int)
+{
+	int	expected[] = {2147483647, -2147483648, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, double_MAX_int)
+{
+	int	expected[] = {2147483647, 2147483647, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, MIN_int)
+{
+	int	expected[] = {-2147483648, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, two_valid_int)
+{
+	int	expected[] = {1, 2, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, three_valid_int)
+{
+	int	expected[] = {1, 2, 3, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, three_valid_negint)
+{
+	int	expected[] = {-1, -2, -3, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, rev_three_valid_negint)
+{
+	int	expected[] = {-3, -2, -1, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
+}
+
+Test(init_stack_a, 0_three_valid_negint)
+{
+	int	expected[] = {-3, 0, -1, '\0'};
+	cr_assert(test_init_stack_a(expected) == true);
+	cr_assert_stdout_eq_str("");
 }
--- unit_test/src/usrcs.mk
+++ unit_test/src/usrcs.mk
@@ -1,1 +1,1 @@
-USRCS := push_swap_tester.c redirect_all_stdout.c assert_empty_stdout.c assert_error_stdout.c
+USRCS := push_swap_tester.c redirect_all_stdout.c

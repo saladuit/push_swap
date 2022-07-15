f3a2a9d6db6f77b0d8f276401d92fd90c94a8295
Saladin Afoh <43225170+saladuit@users.noreply.github.com>
2022-07-15 18:04:58 +0200

    reverted rotate_list

--- libs/libft/src/ft_rotate_list.c
+++ libs/libft/src/ft_rotate_list.c
@@ -6,7 +6,7 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/07/07 20:28:56 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/07/15 17:03:48 by safoh        \___)=(___/                 */
+/*   Updated: 2022/07/15 18:04:46 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 void	ft_rotate_list(t_list **lst)
 {
-	t_list	*first;
-	t_list	*last;
+	t_list	*tmp;
 
-	first = *lst;
-	last = *lst;
-	while (last->next != NULL)
-		last = last->next;
-	*lst = first->next;
-	first->next = NULL;
-	last->next = first;
+	tmp = (*lst)->next;
+	ft_lstadd_back(lst, *lst);
+	*lst = tmp;
 	return ;
 }

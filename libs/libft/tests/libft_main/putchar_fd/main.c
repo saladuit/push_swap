74d593f8b9d18affe104baee3ed03682f35eb123
Saladin Afoh <43225170+saladuit@users.noreply.github.com>
2022-07-15 18:55:54 +0200

    cleaned UP project

--- libs/libft/tests/libft_main/putchar_fd/main.c
+++
@@ -1,9 +0,0 @@
-#include "../libft.h"
-#include "/usr/include/bsd/string.h"
-#include <stdio.h>
-
-int main()
-{
-    ft_putchar_fd('c', 1);
-    ft_putchar_fd('\n', 1);
-}
--- src/main.c
+++ src/main.c
@@ -6,20 +6,14 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/06/27 15:07:11 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/07/15 16:07:00 by safoh        \___)=(___/                 */
+/*   Updated: 2022/07/15 18:35:03 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	leakscheck()
-{
-//	system("leaks push_swap");
-}
-
 int	main(int argc, char **argv)
 {
-//	atexit(leakscheck);
 	if (argc == 1)
 		return (EXIT_SUCCESS);
 	if (push_swap(argc - 1, (const char **)&argv[1]) == false)

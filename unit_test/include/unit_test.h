c1236bf0e0a05033cddcdc7d5c20473d1d3029c5
Saladin Afoh <43225170+saladuit@users.noreply.github.com>
2022-07-16 19:56:56 +0200

    changed some norme errors in unit tester

--- unit_test/include/unit_test.h
+++ unit_test/include/unit_test.h
@@ -6,7 +6,7 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/06/20 17:04:51 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/07/07 14:26:17 by safoh        \___)=(___/                 */
+/*   Updated: 2022/07/16 19:55:09 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,18 @@
 # include "argv_checker.h"
 
 void	redirect_all_std(void);
-void	assert_empty_stdout();
-void	assert_error_stdout();
 
-void	argv_checker_test(const int len, const char **argv, const bool expected);
-void	array_check_test(const int len, const int *integer, const int expected);
+void	argv_checker_test(const int len, const char **argv, \
+		const bool expected);
+void	array_check_test(const int len, const int *integer, \
+		const int expected);
 
-void	init_integer_array_test(const int len, const char **argv, const int *expected);
-void	parse_argv_to_array_test(const int len, const char **argv, const int *expected);
-void	init_stack_test(const int len, const int *array);
 void	make_positive_test(const int len, int *input, const int *expected);
+void	init_integer_array_test(const int len, const char **argv, \
+		const int *expected);
+void	init_stack_test(const int len, const int *array);
 void	sort_array_test(const int len, const int *input, const int *expected);
 
-void	push_top_node_test(const size_t size_a, const size_t size_b, \
-		const int *input_a, const int *input_b, \
-		const int *expected_a, const int *expected_b);
-void	rotate_list_test(const size_t size_a, const int *input_a, const int *expected_a);
-void	rev_rotate_list_test(const size_t size_a, const int *input_a, const int *expected_a);
-void	swap_top_list_test(const size_t size, const int *input, const int *expected);
 void	push_swap_test(const int len, const char **argv, const bool expected);
+
 #endif
--- unit_test/src/push_swap_tester.c
+++ unit_test/src/push_swap_tester.c
@@ -6,7 +6,7 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/06/27 14:45:57 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/07/16 15:35:23 by safoh        \___)=(___/                 */
+/*   Updated: 2022/07/16 19:53:03 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,18 @@ Test(argv_checker, fixed)
 	argv_checker_test(2, ((const char *[]){"1", " "}), false);
 }
 
+Test(array_check, fixed)
+{
+	array_check_test(1, ((const int []){0}), SORTED);
+	array_check_test(2, ((const int []){0, 1}), SORTED);
+	array_check_test(2, ((const int []){1, 0}), NOT_SORTED);
+	array_check_test(2, ((const int []){0, 0}), DOUBLE);
+}
+
 Test(init_integer_array_test, fixed)
 {
 	init_integer_array_test(1, ((const char *[]){"1"}), ((const int []){1}));
 	init_integer_array_test(1, ((const char *[]){"+2147483648"}), NULL);
-/*tests that don't fail the project */
-/*I wasn't able to test the existance of integer array */
 	init_integer_array_test(1, ((const char *[]){"0"}), ((const int []){0}));
 	init_integer_array_test(1, ((const char *[]){"-00"}), ((const int []){0}));
 	init_integer_array_test(1, ((const char *[]){"+00"}), ((const int []){0}));
@@ -48,15 +54,6 @@ Test(init_integer_array_test, fixed)
 	init_integer_array_test(2, ((const char *[]){"1", "2"}), ((const int []){1, 2}));
 }
 
-// Is the list already sorted and does it contain double numbers
-Test(array_check, fixed)
-{
-	array_check_test(1, ((const int []){0}), SORTED);
-	array_check_test(2, ((const int []){0, 1}), SORTED);
-	array_check_test(2, ((const int []){1, 0}), NOT_SORTED);
-	array_check_test(2, ((const int []){0, 0}), DOUBLE);
-}
-
 Test(make_positive, fixed)
 {
 	make_positive_test(1, ((int []){-1}), ((const int []){0}));
@@ -65,53 +62,11 @@ Test(make_positive, fixed)
 	make_positive_test(3, ((int []){2, 3, 1}), ((const int []){1, 2, 0}));
 }
 
-//Test(parse_argv_to_array, fixed)
-//{
-//	parse_argv_to_array_test(1, (const char *[]){"1"}, (const int []){1});
-//	parse_argv_to_array_test(1, (const char *[]){"a"}, NULL);
-//}
-
 Test(init_stack, fixed)
 {
 	init_stack_test(2, ((const int []){1, 2}));
 }
-////////////////////////////////////////////////////////////////////////////////
-//			ACTIONS
-////////////////////////////////////////////////////////////////////////////////
-//Test(ft_swap_nodes_test, fixed, .init=redirect_all_std)
-//{
-//	ft_swap_nodes_test(2, ((const int []){10, 1}), ((const int []){1, 10}));
-//	ft_swap_nodes_test(3, ((const int []){1, 10, 1}), ((const int []){10, 1, 1}));
-//}
 
-//Test(push_top_node, fixed, .init=redirect_all_std)
-//{
-//	push_top_node_test(1, 0, \
-//			((const int []){10}), ((const int []){}), \
-//			((const int []){}), ((const int []){10}));
-//	push_top_node_test(2, 1, \
-//			((const int []){10, 1}), ((const int []){1}), \
-//			((const int []){1}), ((const int []){10, 1}));
-//	push_top_node_test(0, 2, \
-//			((const int []){}), ((const int []){10, 1}), \
-//			((const int []){}), ((const int []){10, 1}));
-//}
-
-//Test(roatate_list, fixed, .init=redirect_all_std)
-//{
-//	rotate_list_test(0, ((const int []){}), ((const int []){}));
-//	rotate_list_test(1, ((const int []){10}), ((const int []){10}));
-//	rotate_list_test(2, ((const int []){10, 1}), ((const int []){1, 10}));
-//	rotate_list_test(3, ((const int []){10, 2, 1}), ((const int []){2, 1, 10}));
-//}
-//
-//Test(rev_rotate_list, fixed, .init=redirect_all_std)
-//{
-//	rev_rotate_list_test(0, ((const int []){}), ((const int []){}));
-//	rev_rotate_list_test(1, ((const int []){10}), ((const int []){10}));
-//	rev_rotate_list_test(2, ((const int []){10, 1}), ((const int []){1, 10}));
-//	rev_rotate_list_test(3, ((const int []){10, 2, 1}), ((const int []){1, 10, 2}));
-//}
 ////////////////////////////////////////////////////////////////////////////////
 //			PROGRAM
 ////////////////////////////////////////////////////////////////////////////////
